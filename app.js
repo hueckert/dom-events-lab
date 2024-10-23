c6236ac996a605e7025bcd8322278036d848fedc
howard <howard.ueckert@protonmail.com>
2024-10-22 20:42:10 -0500

    constants set up

---
+++ app.js
@@ -0,0 +1,27 @@
+//console.log('sanity check')
+
+// USER STORIES
+// As a user, I want to be able to select numbers so that I can perform operations with them.
+// As a user, I want to be able to add two numbers together.
+// As a user, I want to be able to subtract one number from another.
+// As a user, I want to be able to multiply two numbers together.
+// As a user, I want to be able to divide one number by another.
+// As a user, I want to be able to see the output of the mathematical operation.
+// As a user, I want to be able to clear all operations and start from 0.
+
+
+
+/*-------------------------------- Constants --------------------------------*/
+const buttons = document.querySelectorAll('.button');
+const calculator = document.querySelector('#calculator');
+const display = document.querySelector('.display');
+const buttonOperator = document.querySelector('.button operator');
+/*-------------------------------- Variables --------------------------------*/
+
+let firstIput = null
+let
+/*------------------------ Cached Element References ------------------------*/
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+/*-------------------------------- Functions --------------------------------*/
