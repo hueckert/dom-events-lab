537ee681e04a67c00a2241a2363e0daf3941bfda
howard <howard.ueckert@protonmail.com>
2024-10-23 22:01:38 -0500

    some more work

--- app.js
+++ app.js
@@ -9,102 +9,81 @@
 // As a user, I want to be able to see the output of the mathematical operation.
 // As a user, I want to be able to clear all operations and start from 0.
 
-
-
 /*-------------------------------- Constants --------------------------------*/
-const buttons = document.querySelectorAll('.button');
-const calculator = document.querySelector('#calculator');
-const display = document.querySelector('.display');
-const buttonOperator = document.querySelector('.button operator');
+const buttons = document.querySelectorAll(".button");
+const calculator = document.querySelector("#calculator");
+const display = document.querySelector(".display");
+const buttonOperator = document.querySelector(".button operator");
 /*-------------------------------- Variables --------------------------------*/
 
-let firstIput = null
-let secondInput = null
-let operator = null
-let result = null
+let firstIput = null;
+let secondInput = null;
+let operator = null;
+let result = null;
+// display.innerText = 0
 
 /*------------------------ Cached Element References ------------------------*/
 
-// buttons.forEach((button) => {
-//     button.addEventListener('click', (event) => {
-//       // This log is for testing purposes to verify we're getting the correct value
-//      // console.log(event.target.innerText);
-//       // Future logic to capture the button's value would go here...
-//     });
-//   });
-  
-//   calculator.addEventListener('click', (event) => {
-//     // This log is for testing purposes to verify we're getting the correct value
-//     // You have to click a button to see this log
-//     console.log(event.target.innerText);
-  
-//     // Example
-//     if (event.target.classList.contains('number')) {
-//       // Do something with a number
-//     }
-  
-//     // Example
-//     if (event.target.innerText === '*') {
-//       // Do something with this operator
-//     }
-//   });
 
-// ABOVE WAS ALL FOR TESTING PURPOSES
-  
+
 /*----------------------------- Event Listeners -----------------------------*/
 
-calculator.addEventListener('click', (event)=>{
+calculator.addEventListener("click", (event) => {
+   if (event.target.className === "button number") {
+    if (!operator) {
+      if (display.innerText.length < 8) {
+        display.innerText += event.target.innerText;
+        firstIput = +display.innerText;
+      }
+    } else {
+      if (display.innerText.length < 8) {
+        display.innerText += event.target.innerText;
+        secondInput = +display.innerText;
+      }
+    }
+    console.log({ firstIput, secondInput, operator });
+  }
 
-    if (event.target.className === "button number") {
-        if(!operator) {
-            if(display.innerText.length < 15) {
-                display.innerText += event.target.innerText
-                firstIput = +display.innerText
-            }
-        } else {
-            if(display.innerText.length < 15) {
-                display.innerText += event.target.innerText
-                secondInput = +display.innerText
-            }
-        }
-        console.log({firstIput, secondInput, operator})
+  if (event.target.className === "button operator") {
+    operator = event.target.innerText;
+    display.innerText = "";
+    console.log(operator);
+  }
+
+  if (
+    event.target.innerText === "=" &&
+    (firstIput || firstIput === 0) &&
+    (secondInput || secondInput === 0) &&
+    operator
+  ) {
+    if (operator === "/") {
+      result = firstIput / secondInput;
+    }
+    if (operator === "*") {
+      result = firstIput * secondInput;
+    }
+    if (operator === "-") {
+      result = firstIput - secondInput;
+    }
+    if (operator === "+") {
+      result = firstIput + secondInput;
     }
 
-    if (event.target.className === "button operator") {
-        operator = event.target.innerText
-        display.innerText = ""
-        console.log(operator)
+    firstIput = result;
+    if (result.toString().length < 15) {
+      display.innerText = result;
+    } else {
+      display.innerText = result.toPrecision(11);
     }
+    console.log(result);
+  }
 
-    if (event.target.innerText === "=" && (firstIput || firstIput === 0) && (secondInput || secondInput === 0) && operator) {
-        if (operator === "/") {
-            result = firstIput / secondInput
-        }
-        if (operator === "*") {
-            result = firstIput * secondInput
-        }
-        if(operator === "-") {
-            result = firstIput - secondInput
-        }
-        if (operator === "+") {
-            result = firstIput + secondInput
-        }
-
-        firstIput = result
-        if (result.toString().length < 15) {
-            display.innerText = result
-        } else {
-            display.innerText = result.toPrecision(11)
-        }
-        console.log(result)
-    }
-
-    if (event.target.innerText === "C") {
-        result = null
-        firstIput = null
-        secondInput = null
-        operator = null
-    }
-
-})
+  if (event.target.innerText === "C") {
+    // display.innerText = 0
+    result = null;
+    firstIput = null;
+    secondInput = null;
+    operator = null;
+  }
+});
 /*-------------------------------- Functions --------------------------------*/
